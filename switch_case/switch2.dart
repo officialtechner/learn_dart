bac370ec1ea559b66427fc98e12e988fd2dfe354
Sachin negi <sachin@technersolutions.com>
2022-04-26 19:05:57 +0530

    add

--- switch_case/switch2.dart
+++ switch_case/switch2.dart
@@ -1,1 +1,23 @@
-
+void main() {
+  var grade = "A";
+  switch (grade) {
+    case "1":
+      print("Your grade is A");
+      break;
+    case "2":
+      print("Your grade is B");
+      break;
+    case "3":
+      print("Your grade is C");
+      break;
+    case "4":
+      print("Your grade is D");
+      break;
+    case "5":
+      print("Your grade is E");
+      break;
+    case "6":
+      print("Your grade is F");
+      break;
+  }
+}
