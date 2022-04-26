42262294bd2738c6f09d55ee3151a9c29916e8e6
Sachin negi <sachin@technersolutions.com>
2022-04-26 17:12:11 +0530

    add

---
+++ switch_case/switch1.dart
@@ -0,0 +1,35 @@
+void main() {
+  var days = 7;
+  switch (days) {
+    case 1:
+      print("Today is Monday.");
+
+      break;
+    case 2:
+      print("Today is Tuesday.");
+
+      break;
+    case 3:
+      print("Today is Wednesday.");
+
+      break;
+    case 4:
+      print("Today is Thursday.");
+
+      break;
+    case 5:
+      print("Today is Friday.");
+
+      break;
+    case 6:
+      print("Today is Saturdayday.");
+
+      break;
+    case 7:
+      print("Today is Sunday.");
+
+      break;
+    default:
+      print("invalid");
+  }
+}
