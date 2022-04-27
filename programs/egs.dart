5f7917706346c015827e58ae2191bcad981dae88
Sachin negi <sachin@technersolutions.com>
2022-04-27 14:18:11 +0530

    add

---
+++ programs/egs.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the number");
+  int? num = int.parse(stdin.readLineSync()!);
+
+  if (num == 0 || num == 1) {
+    print(" $num is not a prime number.");
+  } else if (num % 2 == 0) {
+    print(" $num is a not prime number.");
+  } else {
+    print(" $num is a Prime number.");
+  }
+}
