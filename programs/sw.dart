1cd55a5f95ef3310fe85da165459babfea1cd060
Sachin negi <sachin@technersolutions.com>
2022-04-28 10:35:25 +0530

    add

---
+++ programs/sw.dart
@@ -0,0 +1,17 @@
+void main() {
+  var days = "Monday";
+  "Tuesday";
+  "Wednesday";
+
+  switch (days) {
+    case "Monday":
+
+    case "Tuesday":
+
+    case "Wednesday":
+      print("hello");
+      break;
+    default:
+      print("invalid");
+  }
+}
