31365d31fda43280690c27489510c1f63b7e7a05
Sachin negi <sachin@technersolutions.com>
2022-04-28 14:20:25 +0530

    add

--- programs/egs.dart
+++ programs/egs.dart
@@ -4,7 +4,7 @@ void main() {
   print("Enter the number");
   int? num = int.parse(stdin.readLineSync()!);
 
-  if (num == 0 || num == 1) {
+  if (num == 0 && num == 1) {
     print(" $num is not a prime number.");
   } else if (num % 2 == 0) {
     print(" $num is a not prime number.");
