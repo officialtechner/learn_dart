b979399de94f1459bbc1979a4132bfecd6ec559a
Sachin negi <sachin@technersolutions.com>
2022-05-02 16:11:52 +0530

    add

---
+++ class/class7.dart
@@ -0,0 +1,34 @@
+abstract class Person {
+  void name();
+  void age();
+}
+
+class Boy extends Person {
+  void name() {
+    print("The name of the Boy's is: Sachin");
+  }
+
+  void age() {
+    print("The age of the Boy's is: 21");
+  }
+}
+
+class Girl extends Person {
+  void name() {
+    print("The name of the Girl's is: Parul");
+  }
+
+  void age() {
+    print("The age of the Girl's is 20");
+  }
+}
+
+void main() {
+  var obj1 = new Boy();
+  obj1.name();
+  obj1.age();
+
+  var obj2 = new Girl();
+  obj2.name();
+  obj2.age();
+}
