c7216520681728cd4910f42cb07437c4541b146e
Sachin negi <sachin@technersolutions.com>
2022-05-02 13:10:11 +0530

    add

---
+++ Lists/intolist.dart
@@ -0,0 +1,9 @@
+void main() {
+  var list = [1, 2];
+  int i = int.parse("3");
+
+  //list.add("3");
+  list.add(i);
+  print(list);
+  print(list.runtimeType);
+}
---
+++ class/class5.dart
@@ -0,0 +1,18 @@
+import 'class1.dart';
+
+void main() {
+  var Student1 = new Student("Sachin", 12, "Chandigrah");
+
+  print("The name of student is:${Student1.name}");
+  print("The id of student is:${Student1.id}");
+  print("The address of student is:${Student1.Address}");
+}
+
+class Student {
+  String name;
+  int id;
+  String Address;
+  Student(this.name, this.id, this.Address) {
+    //Parameterized Constructor.
+  }
+}
